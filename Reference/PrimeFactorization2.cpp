3bfa026216f30e1d6225dff5ff3c5ee720fc1a63
Heon <hjhjaueon97@naver.com>
2022-06-08 23:23:19 +0900

    [update]rename
    소인수분해2

--- Reference/PrimeFactorization2.cpp
+++ Reference/PrimeFactorization2.cpp
@@ -1,29 +1,20 @@
 #include<stdio.h>
-#include<math.h>
-#include<string.h>
-#include<iostream>
-#include<functional>
-#include<string>
 #include<algorithm>
 #include<vector>
-#include<map>
-#include<set>
-#include<queue>
-#include<stdlib.h>
 #include<stack>
 using namespace std;
-const int SIZE=5000009;
-int minfac[SIZE]={0};
+const int SCALE = 5000009;
+int minfac[SCALE]={0};
 
 int main(){
     int n;  scanf("%d",&n);
     minfac[0]=minfac[1]=-1;
     
-    for(int i=2 ; i<SIZE ; i++)
+    for(int i=2 ; i<SCALE ; i++)
         minfac[i]=i;
 
-    for(int i=2 ; i*i<SIZE ; i++){
-        for(int j=i*i ; j<SIZE ; j+=i){
+    for(int i=2 ; i*i<SCALE ; i++){
+        for(int j=i*i ; j<SCALE ; j+=i){
             if(minfac[j]==j)
                 minfac[j]=i;
         }
@@ -34,7 +25,7 @@ int main(){
         vector<int> ans;
         while(inp>1){
             ans.push_back(minfac[inp]);
-            inp/=minfac[inp];
+            inp /= minfac[inp];
         }
         sort(ans.begin(),ans.end());
         for(int here: ans) printf("%d ",here);
